253839c2814faf79d7ce4c98afc6acc07362fd7e
tom-waka <one.sukt.one@gmail.com>
2020-02-11 16:25:32 +0900

    implement articles CRUD

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,41 @@
+class ArticlesController < ApplicationController
+  def index
+    @articles = Article.order(created_at: :desc)
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    article = Article.new(article_params)
+    article.save
+    redirect_to root_url, notice: "記事を投稿しました。"
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    article = Article.find(params[:id])
+    article.update(article_params)
+    redirect_to root_url, notice: "記事を更新しました。"
+  end
+
+  def destroy
+    article = Article.find(params[:id])
+    article.destroy
+    redirect_to root_url, notice: "記事を削除しました。"
+  end
+
+  private
+
+    def article_params
+      params.require(:article).permit(:title, :content)
+    end
+end
