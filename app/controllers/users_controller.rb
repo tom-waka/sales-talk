afa72b52ab52d756cec3a2a51bd43e445b460efe
tom-waka <one.sukt.one@gmail.com>
2020-03-04 07:35:35 +0900

    add user_remove_picture

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -55,7 +55,7 @@ class UsersController < ApplicationController
   private
 
     def user_params
-      params.require(:user).permit(:name, :email, :password, :password_confirmation, :picture)
+      params.require(:user).permit(:name, :email, :password, :password_confirmation, :picture, :remove_picture)
     end
 
     def correct_user
