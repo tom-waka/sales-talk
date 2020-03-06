d71a05dd121d39d0199da1bd9c5b2cfe6b7d31c0
tom-waka <one.sukt.one@gmail.com>
2020-03-06 23:14:56 +0900

    updating layouts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
   
 
   def index
-    @users = User.order(:created_at)
+    @users = User.order(:created_at).page(params[:page]).per(20)
   end
 
   def show
