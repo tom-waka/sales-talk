5182eb98b23a51e53853f47847e1ec7916776f11
tom-waka <one.sukt.one@gmail.com>
2020-03-12 21:57:20 +0900

    article関係のsystem specは一旦リセット

--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -1,6 +1,5 @@
 FactoryBot.define do
   factory :article do
-    sequence(:id)  { |n| "#{n}"}
     title {"title"}
     content {"content"}
     user
--- spec/system/articles_spec.rb
+++ spec/system/articles_spec.rb
@@ -1,29 +1,5 @@
 require 'rails_helper'
 
-RSpec.describe "Articles", type: :system do
-
-  let(:user) {create(:user)}
-  let(:article) {build(:article)}
-
-  it "ユーザーが新しい記事を投稿" do
-    login(user)
-    visit new_article_path
-    fill_in 'article[title]', with: article.title
-    fill_in "article[content]", with: article.content
-    click_button "投稿する"
-    expect(page).to have_content "記事を投稿しました" 
-    expect(Article.count).to eq 1
-  end
-
-  it "ユーザーが記事を編集" do
-    login(user)
-    visit edit_article_path(article)
-    fill_in 'article[title]', with: "編集後のタイトル"
-    fill_in "article[content]", with: "編集後のコンテント"
-    click_button "更新する"
-    expect(page).to have_content "記事を更新しました" 
-    expect(current_path).to eq article_path(article)
-    expect(Article.count).to eq 1
-  end
+RSpec.describe 'Articles', type: :system do
 
 end
