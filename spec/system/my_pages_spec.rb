a7534dd58fa5c9e0077d95a0f1fe261a7c25aa37
tom-waka <one.sukt.one@gmail.com>
2020-03-14 08:25:10 +0900

    my_pages_specの更新

--- spec/system/my_pages_spec.rb
+++ spec/system/my_pages_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe "MyPages", type: :system do
   describe 'マイページの挙動' do
     let(:user_1) {create(:user, name: 'ユーザー1', email: 'user1@sample.com')}
     let(:user_2) {create(:user, name: 'ユーザー2', email: 'user2@sample.com')}
-    describe 'マイページの表示' do
+    context 'マイページの表示' do
       it '表示成功' do
         login_as(user_1)
         visit user_path(user_1)
@@ -13,32 +13,54 @@ RSpec.describe "MyPages", type: :system do
     end
 
     describe '登録情報の編集' do
-      it '編集成功' do
-        login_as(user_1)
-        visit edit_user_path(user_1)
-        fill_in 'メールアドレス', with: 'edit@sample.com'
-        click_button '更新する'
-        expect(page).to have_content 'ユーザー情報を更新しました'
-        expect(current_path).to eq(user_path(user_1))
+      context '自分の登録情報を編集' do
+        it '編集成功' do
+          login_as(user_1)
+          visit edit_user_path(user_1)
+          fill_in 'メールアドレス', with: 'edit@sample.com'
+          click_button '更新する'
+          expect(page).to have_content 'ユーザー情報を更新しました'
+          expect(current_path).to eq(user_path(user_1))
+        end
       end
 
-      it '別ユーザーは編集不可' do
-        login_as(user_2)
-        visit edit_user_path(user_1)
-        expect(page).to have_content 'このURLにはアクセスできません'
-        expect(current_path).to eq(root_path)
+      context '別ユーザーを編集しようとした時' do
+        it '編集リンクの表示なし' do
+          login_as(user_2)
+          visit user_path(user_1)
+          expect(current_path).to eq(user_path(user_1))
+          expect(page).to have_no_content '登録情報を編集する'
+        end
+
+        it '編集ページへアクセス不可' do
+          login_as(user_2)
+          visit edit_user_path(user_1)
+          expect(page).to have_content 'このURLにはアクセスできません'
+          expect(current_path).to eq(root_path)
+        end
       end
     end
 
-    describe '退会' do
-      it '退会成功' do
-        login_as(user_1)
-        visit user_path(user_1)
-        click_link '退会する'
-        page.driver.browser.switch_to.alert.accept
-        expect(current_path).to eq(root_path)
-        expect(page).to have_content 'ユーザー「ユーザー1」は削除されました'
-        expect(User.count).to eq 0
+    describe 'ユーザーの削除' do
+      context '退会しようとした時' do
+        it '退会成功' do
+          login_as(user_1)
+          visit user_path(user_1)
+          click_link '退会する'
+          page.driver.browser.switch_to.alert.accept
+          expect(current_path).to eq(root_path)
+          expect(page).to have_content 'ユーザー「ユーザー1」は削除されました'
+          expect(User.count).to eq 0
+        end
+      end
+
+      context '別ユーザーを退会させようとした時' do
+        it '退会リンクの表示なし' do
+          login_as(user_2)
+          visit user_path(user_1)
+          expect(current_path).to eq(user_path(user_1))
+          expect(page).to have_no_content '退会する'
+        end
       end
     end
   end
