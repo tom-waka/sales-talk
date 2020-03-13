bccd1cbb3163c1158d24e8916f9ad060b78e52ba
tom-waka <one.sukt.one@gmail.com>
2020-03-13 22:38:05 +0900

    ログインヘルパーの追加

---
+++ spec/support/login.rb
@@ -0,0 +1,9 @@
+module LoginHelpers
+  def login_as(user)
+    visit login_path
+    fill_in 'メールアドレス', with: user.email
+    fill_in 'パスワード', with: 'foobar'
+    click_button 'ログインする'
+    expect(page).to have_content 'ログインしました'    
+  end
+end
--- spec/system/articles_spec.rb
+++ spec/system/articles_spec.rb
@@ -1,23 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe 'Articles', type: :system do
-  describe '記事関連の挙動' do
+  describe '記事のCRUD' do
     let (:user_1) {create(:user, name: 'ユーザー1', email: 'user1@sample.com')}
     let (:user_2) {create(:user, name: 'ユーザー2', email: 'user2@sample.com')}
     let (:article_1) {create(:article, title: 'タイトル1', user: user_1)}
 
-    before do
-      visit login_path
-      fill_in 'メールアドレス', with: login_user.email
-      fill_in 'パスワード', with: 'foobar'
-      click_button 'ログインする'
-      expect(page).to have_content 'ログインしました'
-    end
 
     describe '記事の新規投稿' do
-      let(:login_user){ user_1 }
-      context 'フォームが正しい場合' do
+      context '成功時の挙動' do
         it '投稿成功' do
+          login_as(user_1)
           visit new_article_path
           fill_in 'article[title]', with: 'タイトルを入力'
           fill_in 'article[content]', with: '内容を入力'
@@ -26,19 +19,25 @@ RSpec.describe 'Articles', type: :system do
         end
       end
 
-      context 'フォームが正しくない場合' do
+      context '失敗時の挙動' do
         it 'タイトル未入力のため投稿失敗' do
+          login_as(user_1)
           visit new_article_path
           fill_in 'article[title]', with: ''
           fill_in 'article[content]', with: '内容を入力'
           click_button '投稿する'
           expect(page).to have_content 'タイトルを入力してください'
         end
+
+        it 'ログイン前のため失敗' do
+          visit new_article_path
+          expect(current_path).to eq(login_path)
+          expect(page).to have_content 'ログインをしてください'
+        end
       end
     end
 
     describe '記事の詳細表示' do
-      let(:login_user){ user_1 }
       context '記事が存在する場合' do
         it '表示成功' do
           visit article_path(article_1)
@@ -48,17 +47,44 @@ RSpec.describe 'Articles', type: :system do
     end
       
     describe '記事の編集' do
-      context 'ユーザーがログインしている場合' do
-        let(:login_user){ user_1 }
+      context 'ログインしている場合' do
         it '編集成功' do
+          login_as(user_1)
           visit edit_article_path(article_1)
           fill_in 'article[title]', with: 'タイトルを編集'
           fill_in 'article[content]', with: '内容を編集'
           click_button '更新する'
           expect(page).to have_content '記事を更新しました'
         end
+
+        it '他ユーザーの記事は編集不可' do
+          login_as(user_2)
+          visit edit_article_path(article_1)
+          expect(current_path).to eq(root_path)
+          expect(page).to have_content 'このURLにはアクセスできません'
+        end
       end
 
+      context 'ログイン前' do
+        it '編集ページへアクセス不可' do
+          visit edit_article_path(article_1)
+          expect(current_path).to eq(login_path)
+          expect(page).to have_content 'ログインをしてください'
+        end
+      end
     end
+
+    describe '記事の削除' do
+      context 'ログインしている場合' do
+        it '削除成功' do
+          login_as(user_1)
+          visit article_path(article_1)
+          click_link('削除')
+          page.driver.browser.switch_to.alert.accept
+          expect(page).to have_content '記事を削除しました'
+        end
+      end
+    end
+
   end
 end
