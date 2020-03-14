c7d82c3c9e31f28fe38e8c8524f95686761d9c4f
tom-waka <one.sukt.one@gmail.com>
2020-03-14 11:30:37 +0900

    my_pages_specにadmin関連のテストを追加

--- spec/system/my_pages_spec.rb
+++ spec/system/my_pages_spec.rb
@@ -2,8 +2,10 @@ require 'rails_helper'
 
 RSpec.describe "MyPages", type: :system do
   describe 'マイページの挙動' do
-    let(:user_1) {create(:user, name: 'ユーザー1', email: 'user1@sample.com')}
-    let(:user_2) {create(:user, name: 'ユーザー2', email: 'user2@sample.com')}
+    let (:user_1) {create(:user, name: 'ユーザー1', email: 'user1@sample.com')}
+    let (:user_2) {create(:user, name: 'ユーザー2', email: 'user2@sample.com')}
+    let (:admin_user) {create(:user, name: 'アドミン', email: 'admin@sample.com', admin: 'true')}
+    
     context 'マイページの表示' do
       it '表示成功' do
         login_as(user_1)
@@ -29,7 +31,7 @@ RSpec.describe "MyPages", type: :system do
           login_as(user_2)
           visit user_path(user_1)
           expect(current_path).to eq(user_path(user_1))
-          expect(page).to have_no_content '登録情報を編集する'
+          expect(page).to have_no_link '登録情報を編集する'
         end
 
         it '編集ページへアクセス不可' do
@@ -39,6 +41,18 @@ RSpec.describe "MyPages", type: :system do
           expect(current_path).to eq(root_path)
         end
       end
+
+      context 'adminでログインした時' do
+        it '編集成功' do
+          login_as(admin_user)
+          visit edit_user_path(user_1)
+          fill_in 'メールアドレス', with: 'edit@sample.com'
+          click_button '更新する'
+          expect(page).to have_content 'ユーザー情報を更新しました'
+          expect(current_path).to eq(user_path(user_1))
+        end
+      end
+
     end
 
     describe 'ユーザーの削除' do
@@ -59,7 +73,19 @@ RSpec.describe "MyPages", type: :system do
           login_as(user_2)
           visit user_path(user_1)
           expect(current_path).to eq(user_path(user_1))
-          expect(page).to have_no_content '退会する'
+          expect(page).to have_no_button '退会する'
+        end
+      end
+
+      context 'adminでログインした時' do
+        it '削除成功' do
+          login_as(admin_user)
+          visit user_path(user_1)
+          click_link 'ユーザーを削除する'
+          page.driver.browser.switch_to.alert.accept
+          expect(current_path).to eq(users_path)
+          expect(page).to have_content 'ユーザー「ユーザー1」は削除されました'
+          expect(User.count).to eq 1
         end
       end
     end
