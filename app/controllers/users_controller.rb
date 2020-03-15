3fd67b1b81564645f8160361da4f7e39d90aa56b
tom-waka <one.sukt.one@gmail.com>
2020-03-15 17:56:06 +0900

    営業履歴のリファクタリング

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @user_articles = @user.articles.page(params[:page]).per(6)
   end
   
   def new
