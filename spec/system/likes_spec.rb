747b9fcbf3474777d5c80a8292c50a112bf243d8
tom-waka <one.sukt.one@gmail.com>
2020-03-15 09:56:34 +0900

    いいねテストの追加

---
+++ spec/system/likes_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe "Likes", type: :system do
+  describe 'いいねのテスト' do
+    let (:user){create(:user, name: 'ユーザー1', email: 'user1@sample.com')}
+    let (:article){create(:article)}
+    context 'ログイン時' do
+      it 'いいね成功' do
+        login_as(user)
+        visit article_path(article)
+        find('.fa-heart').click
+        expect(page).to have_selector '.counter', text: '1'
+      end
+    end
+  end
+end
