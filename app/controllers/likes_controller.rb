15eb5c7f7d0b9d1494eac26b450ed5b981ca93f8
tom-waka <one.sukt.one@gmail.com>
2020-03-20 20:17:03 +0900

    @article.reloadの追記

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -5,11 +5,13 @@ class LikesController < ApplicationController
     @article = Article.find(params[:article_id])
     liked = current_user.likes.build(article_id: params[:article_id])
     liked.save
+    @article.reload
   end
 
   def destroy
     @article = Article.find(params[:article_id])
     liked = Like.find_by(article_id: params[:article_id], user_id: current_user.id)
     liked.destroy
+    @article.reload
   end
 end
