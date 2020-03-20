599042ce9aabc31fe7e9407fdc05187e87214c00
tom-waka <one.sukt.one@gmail.com>
2020-03-21 08:21:27 +0900

    categoryを追加

--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -2,6 +2,7 @@ FactoryBot.define do
   factory :article do
     title {"title"}
     content {"content"}
+    category
     user
   end
 end
--- spec/factories/categories.rb
+++ spec/factories/categories.rb
@@ -1,5 +1,5 @@
 FactoryBot.define do
   factory :category do
-    name { "MyString" }
+    name { "本" }
   end
 end
