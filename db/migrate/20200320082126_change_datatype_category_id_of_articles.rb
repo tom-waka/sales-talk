b19a4c2533cbea9cbb46b5e57311cc02e08a57ed
tom-waka <one.sukt.one@gmail.com>
2020-03-22 11:47:17 +0900

    マイグレーションファイルの修正

--- db/migrate/20200320082126_change_datatype_category_id_of_articles.rb
+++ db/migrate/20200320082126_change_datatype_category_id_of_articles.rb
@@ -1,5 +1,5 @@
 class ChangeDatatypeCategoryIdOfArticles < ActiveRecord::Migration[5.2]
   def change
-    change_column :articles, :category_id, 'integer USING CAST(user_id AS integer)'
+    change_column :articles, :category_id, 'integer USING CAST(category_id AS integer)'
   end
 end
