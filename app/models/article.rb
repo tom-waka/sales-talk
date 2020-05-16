067a9a642643dca072fe43c8bd6030c2545087d8
tom-waka <one.sukt.one@gmail.com>
2020-05-16 18:24:20 +0900

    rubocopの指摘箇所を修正

--- app/models/article.rb
+++ app/models/article.rb
@@ -4,11 +4,10 @@ class Article < ApplicationRecord
   has_many :likes, dependent: :destroy
   default_scope -> { order(created_at: :desc) }
   validates :title, presence: true
-  validates :title, length: {maximum: 50}
+  validates :title, length: { maximum: 50 }
   validates :user_id, presence: true
 
   def liked_by?(user)
     likes.where(user_id: user.id).exists?
   end
-
 end
